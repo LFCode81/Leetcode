a60cdd53eadf1e28fb111543aecb721e43ea9ebf
LFCode81 <144384718+LFCode81@users.noreply.github.com>
2025-10-21 17:08:16 -0400

    Solved TwoSum

--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,24 @@
 class Solution(object):
-    def twoSum(self,nums,target):
-        pass
+    def twoSum(self,nums,target): # return the indices       
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:  
+                    result =[i,j]     
+                    return result
+                
+
+
+                    
+        
+    
+ 
+nums = [3,2,4]
+#nums = [2,5,5,11] # Rule out same element
+target = 6
+
+
+result = Solution()
+
+
+print(result.twoSum(nums,target))
+
